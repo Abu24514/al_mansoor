afed44398c9c8a429ba20badfacd233a2c136a25
Abu Hanjala <abuhanjala656@gmail.com>
2026-09-05 04:16:27 +0530

    modify page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CTABanner from "@/components/home/CTABanner";
 import FAQ from "@/components/home/FAQ";
 import FeaturedJobs from "@/components/home/FeaturedJobs";
 import ForCompanies from "@/components/home/ForCompanies";
-import Hero from "@/components/home/Hero";
+import HomeHero from "@/components/home/HomeHero";
 import HowItWorks from "@/components/home/HowItWorks";
 import JobCategories from "@/components/home/JobCategories";
 import TrustStats from "@/components/home/TrustStats";
@@ -11,7 +11,7 @@ import WhyChooseUs from "@/components/home/WhyChooseUs";
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <HomeHero/>
       <TrustStats />
       <HowItWorks/>
       <JobCategories />
