a21d618abae7331c4c0b8a20abbdd81a9caaf651
Abu Hanjala <abuhanjala656@gmail.com>
2026-09-05 05:05:05 +0530

    add about and contact page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+
+    return(
+        <section className="h-screen w-full flex justify-center items-center">
+            <h2 className="text-7xl text-emerald-700">Coming Soon...</h2>
+        </section>
+    )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage(){
+    return (
+         <section className="h-screen w-full flex justify-center items-center">
+            <h2 className="text-7xl text-black">Coming Soon...</h2>
+        </section>
+    )
+}
